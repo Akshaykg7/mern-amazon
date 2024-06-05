4d7e3bf563c25bdff998fcaa7764670d9454476f
Akshay K G <akshaygopz@gmail.com>
2024-06-05 17:07:51 +0530

    Managed State By Reducer Hook

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,42 @@
+import React from 'react'
+import { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios'
+import logger from 'use-reducer-logger'
 // import data from "../data";
-import { useEffect, useState } from "react";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true};
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+      default:
+        return state;
+  }
+}
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error:'',
+  });
+
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({type:'FETCH_SUCCESS', payload: result.data })
+      } catch (err) {
+        dispatch({type: 'FETCH_FAIL', payload: err.message });
+      }
+      
+      // setProducts(result.data);
     };
     fetchData();
   }, []);
@@ -21,7 +49,12 @@ function HomeScreen() {
 
 <div className="products">
 
-{products.map((product) => (
+{loading ? (
+  <div>Loading...</div>
+) : error ? (
+  <div>{error}</div>
+) : (
+products.map((product) => (
 
     <div className="product" key={product.slug}>
       <Link to={`/product/${product.slug}`}>
@@ -38,7 +71,7 @@ function HomeScreen() {
       </div>
     </div>
   ))
-  }
+  )}
   </div>
 
     </div>
