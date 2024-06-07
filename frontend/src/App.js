d2d1a2f1bc6159d4916330bce06670dbe8b145dd
Akshay K G <akshaygopz@gmail.com>
2024-06-07 12:19:11 +0530

    Fixed FrontEnd Errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,82 +1,62 @@
-import React, { useContext } from 'react'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+
+
+
+
+
+import React, { useContext } from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import Navbar from 'react-bootstrap/Navbar'
-import Badge from 'react-bootstrap/Badge'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
-import { LinkContainer } from 'react-router-bootstrap'
+import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import { Store } from './Store';
-import CardText from 'react-bootstrap/esm/CardText';
 
 function App() {
-
   const { state } = useContext(Store);
   const { cart } = state;
 
   return (
-    
     <BrowserRouter>
+      <div className='d-flex flex-column side-container'>
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Link to="/" className='Home'>  {}
+                <Navbar.Brand>amazon</Navbar.Brand>
+              </Link>
+              <Nav className="mt-3">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
 
-    <div className='d-flex flex-column side-container'>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
 
-    <header>
-
-      <Navbar bg="dark" variant="dark">
-
-        <Container>
-          <LinkContainer to="/">
-
-            <Navbar.Brand>amazon</Navbar.Brand>
-
-            </LinkContainer>
-
-                <Nav className="mt-3">
-                    <Link to="/cart" className="nav-link">
-                    Cart
-                      {CardText.cartItems.length > 0 && (
-                        <Badge pill bg="danger">
-                          {cart.cartItems.length}
-                        </Badge>
-                      )}
-                    </Link>
-                </Nav>
-
-        </Container>
-
-      </Navbar>
-
-      
-
-    </header>
-
-
-    <main>
-      <Container className="mt-3">
-      <Routes className="homeScreen">
-
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-
-      </Routes>
-      </Container>
-    </main>
-
-    <footer>
-
-      <div className='text-center'>All rights reserved</div>
-
-    </footer>
-
-  </div>
-
-  </BrowserRouter>
-
-
-      );
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
 
