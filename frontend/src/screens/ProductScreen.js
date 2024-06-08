4c43cb01fe740f4ecd169c0c1834d811c2ead0c6
Akshay K G <akshaygopz@gmail.com>
2024-06-08 18:44:14 +0530

    Improve Add To Cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { useContext, useEffect, useReducer } from 'react'
 import { useParams } from "react-router-dom";
 import Row from 'react-bootstrap/Row'
@@ -13,6 +12,7 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
 import { Store } from '../Store';
+import axios from 'axios';
 
 
 
@@ -52,10 +52,19 @@ function ProductScreen() {
         fetchData();
     }, [slug]);
 
-    const { dispatch: ctxDispatch} = useContext(Store);
-    const addToCartHandler = () => {
-        ctxDispatch({type:'CART_ADD_ITEM',
-             payload: {...product, quantity: 1},
+    const {state, dispatch: ctxDispatch} = useContext(Store);
+    const {cart} = state;
+    const addToCartHandler = async() => {
+        const existItem = cart.cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${product._id}`);
+        if (data.countInStock < quantity) {
+            window.alert('Sorry. Product  is out of stock');
+            return;
+        }
+        ctxDispatch({
+            type:'CART_ADD_ITEM',
+            payload: {...product, quantity},
             });
     };
 
